19e4dfb2437696f3a31876d61a210d068886abfe
Valery3dm <jew.3dm@gmail.com>
2020-08-20 23:45:55 +0300

    Updates

---
+++ precache-manifest.0cb04f09d217ae688316a957a7c61de1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20df2090e86993098944126027a17ca5",
+    "url": "/ToDoList.git/index.html"
+  },
+  {
+    "revision": "95990d40fdc58f70b26a",
+    "url": "/ToDoList.git/static/css/main.0e92e1b9.chunk.css"
+  },
+  {
+    "revision": "4560886a3cb028e7a3be",
+    "url": "/ToDoList.git/static/js/2.8a586615.chunk.js"
+  },
+  {
+    "revision": "e2ba2a472913caa27f1c",
+    "url": "/ToDoList.git/static/js/3.8f574fe2.chunk.js"
+  },
+  {
+    "revision": "95990d40fdc58f70b26a",
+    "url": "/ToDoList.git/static/js/main.ebd9c53c.chunk.js"
+  },
+  {
+    "revision": "ce5ddcf23f1487522ab0",
+    "url": "/ToDoList.git/static/js/runtime~main.571cedb3.js"
+  }
+]);
